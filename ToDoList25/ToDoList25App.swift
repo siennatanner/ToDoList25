e9abc456bb689ded0405a14a819f3ef7356d9d51
Sienna Baird <siennatanner21@icloud.com>
2025-05-06 15:19:22 -0700

    Initial Commit

---
+++ ToDoList25/ToDoList25App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList25App.swift
+//  ToDoList25
+//
+//  Created by Sienna Tanner on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
